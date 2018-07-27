7882effc567a9001cd9f4eb55cc194bfad7ab7ff
Jared Lander <info@jaredlander.com>
2018-07-27 11:55:49 -0400

    Started navbarPage

--- app/ui.r
+++ app/ui.r
@@ -1,1 +1,13 @@
 library(shiny)
+
+navbarPage(
+    title='Our First Shiny App',
+    tabPanel(
+        title='First Page',
+        'Hi'
+    ),
+    tabPanel(
+        title='Second Page',
+        'Hello, there'
+    )
+)
