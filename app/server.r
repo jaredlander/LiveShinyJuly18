f745de6aae3eac2e1f3ff8e46b102496ca276803
Jared Lander <info@jaredlander.com>
2018-07-27 12:20:23 -0400

    Created and input and rendered it to output

--- app/server.r
+++ app/server.r
@@ -2,5 +2,6 @@ library(shiny)
 
 shinyServer(function(input, output, session){
     
+    output$DisplayState <- renderText(input$StateSelector)
     
 })
--- app/ui.r
+++ app/ui.r
@@ -2,6 +2,7 @@ library(shiny)
 
 navbarPage(
     title='Our First Shiny App',
+    selected='State Info',
     tabPanel(
         title='First Page',
         'Hi'
@@ -9,5 +10,16 @@ navbarPage(
     tabPanel(
         title='Second Page',
         'Hello, there'
+    ),
+    tabPanel(
+        title='State Info',
+        selectInput(
+            inputId='StateSelector',
+            label='Choose a state',
+            choices=list('NY', 'NJ', 'MA', 'TX')
+        ),
+        textOutput(
+            outputId='DisplayState'
+        )
     )
 )
